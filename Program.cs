16f0592570fd71efca9af5989447ab5168d11e36
Allyson Oliveira <asoliveira@outlook.com>
2021-09-16 16:25:52 -0300

    Edit deployment.yaml

--- Program.cs
+++ Program.cs
@@ -7,12 +7,12 @@ namespace NetCore.Docker
     {
         static async Task Main(string[] args)
         {
-            var counter = 0;
+            var counter = 100;
             var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
             while (max == -1 || counter < max)
             {
                 Console.WriteLine($"Counter: {++counter}");
-                await Task.Delay(1000);
+                await Task.Delay(60000);
             }
         }
     }
